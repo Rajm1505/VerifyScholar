00174fe668afb3f742c3aaa66d7f01c4170829e4
JATIN NABHOYA <jnabhoya474@rku.ac.in>
2022-07-17 14:24:19 +0530

    making a register form and create routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes,Route} from 'react-router-dom';
+import Home from './Home';
+import Login from './login/Login';
+import Register from './login/Register';
+import Contact_Us from './login/Contact_us';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //   </header>
+    // </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login/' element={<Login /> } />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/Contact Us' element={<Contact_Us/>} />
+    </Routes>
+    </>
   );
 }
 
