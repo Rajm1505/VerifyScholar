25aa3fe9d1a158e6e4bd097241454f94f18f9116
Rajm1505 <rmandaviya641@rku.ac.in>
2022-07-17 13:33:56 +0530

    Api for studentDetails created in django

--- api/Serializers.py
+++ api/Serializers.py
@@ -4,7 +4,7 @@ from .models import StudentDetails
 class StudentDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentDetails
-        fields = ['nsp_id_radio','nsp_id','pms_benificiary_id_radio',
+        fields = ['sid','nsp_id_radio','nsp_id','pms_benificiary_id_radio',
         'pms_benificiary_id','caste_category','name','fname','gender','mobile_number',
         'emailid','dob','password','state_of_passing_10th_exam','board_10th_certificate_number',
         'year_of_passing_10th_board']
