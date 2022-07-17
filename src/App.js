2378af3acc00dd8359f313944774f46820244ca0
JATIN NABHOYA <jnabhoya474@rku.ac.in>
2022-07-17 15:44:12 +0530

    register form,api fetch and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes,Route} from 'react-router-dom';
+import Home from './Home';
+import Login from './login/Login';
+import Register from './login/Register';
+import Contact_Us from './login/Contact_us';
+
 
 function App() {
   return (
+<<<<<<< HEAD
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //   </header>
+    // </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login/' element={<Login /> } />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/Contact Us' element={<Contact_Us/>} />
+    </Routes>
+    </>
+=======
     <div className="App">
       <h1>Hello this is VerifyScholar</h1>
       <h3>Our website opop</h3>
     </div>
+>>>>>>> master
   );
 }
 
